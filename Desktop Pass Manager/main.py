36a241cc4d2d38e1111455c9672af00ae91bbac8
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-29 15:30:33 +0200

    Add strong password generator function.

--- Desktop Pass Manager/main.py
+++ Desktop Pass Manager/main.py
@@ -6,6 +6,30 @@ from random import randint, shuffle, choice
 import pyperclip
 import re
 
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def gen_pass():
+    """This function generate strong password."""
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = [choice(letters) for _ in range(randint(8, 11))]
+    password_list += [choice(symbols) for _ in range(randint(3, 4))]
+    password_list += [choice(numbers) for _ in range(randint(3, 4))]
+
+    shuffle(password_list)
+    password = "".join(password_list)
+
+    entry_pass.insert(0, password)
+
+    pyperclip.copy(password)
+
+    label_copied.config(text="Copied!")
+
+
 DB_PASS_FILE_PATH = "./pass-data.txt"
 
 root = Tk()
