6df6be57fa0644c58dccab052333d3dc9fc37532
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-07-26 15:56:50 +0200

    Add necessary imports to main.py

---
+++ Flash Cards App/main.py
@@ -0,0 +1,9 @@
+from functools import partial
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import messagebox
+from playsound import playsound
+from settings import *
+import random
+import csv
+import sys
