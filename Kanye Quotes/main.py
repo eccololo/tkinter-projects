9378a16a852b55a625b5f98ec01a4648fb92da43
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-08-28 15:07:08 +0200

    Kayne Quotes Project - Done.

--- Kanye Quotes/main.py
+++ Kanye Quotes/main.py
@@ -1,9 +1,35 @@
 from tkinter import *
+import requests
+
+WINDOW_WIDTH = 300
+WINDOW_HEIGHT = 414
+
+
+def center_the_project_window(w_root):
+    """This function centers app window of the center of the screen when it is open. Code taken from
+    StackOver Flow."""
+    w = WINDOW_WIDTH  # width for the Tk root
+    h = WINDOW_HEIGHT  # height for the Tk root
+
+    # get screen width and height
+    ws = w_root.winfo_screenwidth()  # width of the screen
+    hs = w_root.winfo_screenheight()  # height of the screen
+
+    # calculate x and y coordinates for the Tk root window
+    x = (ws / 2) - (w / 2)
+    y = (hs / 2) - (h / 2)
+
+    # set the dimensions of the screen
+    # and where it is placed
+    w_root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
 
 def get_quote():
-    pass
-    #Write your code here.
+    # Write your code here.
+    response = requests.get("https://api.kanye.rest")
+    data = response.json()
+    quote = data['quote']
+    canvas.itemconfig(quote_text, text=quote)
 
 
 
@@ -11,16 +37,15 @@ window = Tk()
 window.title("Kanye Says...")
 window.config(padx=50, pady=50)
 
-canvas = Canvas(width=300, height=414)
+canvas = Canvas(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 20, "bold"),
+                                fill="white")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
-
-
 
 window.mainloop()
